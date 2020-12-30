e81260159cadc5d70ff0bf344dbedae8c25a063f
Bari Fuchs <bari.fuchs@gmail.com>
2020-12-30 16:12:49 -0500

    Swapped out between() when counting events because it was all inclusive

--- Event_in_interval.R
+++ Event_in_interval.R
@@ -1,9 +1,10 @@
-# The purpose of this script is define and use event_in_interval() to make time-series data to indicate if an event occured within a specfied interval
+# The purpose of this script is define and use event_in_interval() to make time-series data to indicate (1) if an event occured within a specfied interval and (2) how many events occured within each specific interval
 # Input into event_in_interval() will be a file containing a "StartTime_seconds_adjusted" column (in seconds) that indicates each time the event occured
 # Input files can be generated using Gen_files_for_EventInt.R
 
 library(plyr)
 library(dplyr)
+library(data.table)
 
 #### Import files ####
 # set directory
@@ -15,7 +16,7 @@ file_names = list.files(pattern="*meal*", full.names = TRUE)
 files = lapply(file_names, read.csv, header = TRUE)
 
 # Load 1 file for testing script
-AUR0029.1 <- read.csv("AUR0029.1_meal_bitesonly.csv", stringsAsFactors = FALSE)
+#AUR0029.1 <- read.csv("AUR0029.1_meal_bitesonly.csv", stringsAsFactors = FALSE)
 
 #### Define function for defining event (bite) occurance ####
 # Function will: 
@@ -50,16 +51,28 @@ event_in_interval <- function(data, interval) {
   # Make variable to indicate event count
   Interval.df$EventCount = NA
 
-  # For each interval, Event = TRUE if there is any event in Input_clean that occured between upper and lower threshold for that interval
+  # For each interval, EventOccur = TRUE if there is any event in input data that occured between lower and upper threshold for that interval
   Intnums = unique(Interval.df$IntNum)
   for (Int in Intnums) {
     upperthresh = (Interval.df$upperthresh[Interval.df$IntNum == Int])
     lowerthresh = (Interval.df$lowerthresh[Interval.df$IntNum == Int])
     Interval.df$EventOccur[Interval.df$IntNum == Int] = (any(data$StartTime_seconds_adjusted >= lowerthresh & data$StartTime_seconds_adjusted < upperthresh))
     eventcount = 0
+    lastrow = nrow(data)
+    
+    # Count how many events occured between lower and upperthresholds for each interval
     for (eventnum in 1:nrow(data)) {
-      if (isTRUE(between(data$StartTime_seconds_adjusted[eventnum], lowerthresh, upperthresh))) {
-        eventcount = eventcount + 1
+      if (eventnum != lastrow) {
+        # For all intervals except the last, do not include the upper boundary
+        if (isTRUE(data$StartTime_seconds_adjusted[eventnum] >= lowerthresh & data$StartTime_seconds_adjusted[eventnum] < upperthresh)) {
+          eventcount = eventcount + 1
+        }
+      }  
+      if (eventnum == lastrow) {
+        # For the last interval, include the upper boundary
+        if (isTRUE(data$StartTime_seconds_adjusted[eventnum] >= lowerthresh & data$StartTime_seconds_adjusted[eventnum] <= upperthresh)) {
+          eventcount = eventcount + 1
+        }
       }
     }
     Interval.df$EventCount[Interval.df$IntNum == Int] = eventcount
@@ -67,10 +80,10 @@ event_in_interval <- function(data, interval) {
   return(Interval.df)
 }
 
-#### Apply function; output "EventInInt" dataframes for each meal ####
+#### Apply function; output individual or compiled dataframes ####
 
 # Set desired intervals
-intlist <- list(2, 4)
+intlist <- list(30, 60)
 
 for (interval in intlist) { 
   # Make temporary empty dataframe that all data will be added to
