98414715a56c3f0408e82c5c5c5269803979c5a8
Bari Fuchs <bari.fuchs@gmail.com>
2021-01-18 17:18:01 -0500

    Add optional IBI argument to calculate IBI over the specified interval
    Merges previous Event_in_interval() with IBI_in_interval() using IBI = TRUE argument

--- Event_in_interval.R
+++ Event_in_interval.R
@@ -1,5 +1,10 @@
-# The purpose of this script is define and use event_in_interval() to make time-series data to indicate (1) if an event occured within a specfied interval and (2) how many events occured within each specific interval
+# The purpose of this script is define and use event_in_interval() to make time-series data to indicate:
+    # (1) if an event occured within a specfied interval 
+    # (2) how many events occured within each specific interval
+    # (3) The average IBI during that interval
+
 # Input into event_in_interval() will be a file containing a "StartTime_seconds_adjusted" column (in seconds) that indicates each time the event occured
+# If IBI option is used, input file also requires IBI column
 # Input files can be generated using Gen_files_for_EventInt.R
 
 library(plyr)
@@ -12,8 +17,11 @@ basedir <- ("~/OneDrive - The Pennsylvania State University/Bari-files-Manual-Bo
 setwd(file.path(basedir,"Data/Split/BitesOnly/"))
 
 # Load files
-file_names = list.files(pattern="*meal*", full.names = TRUE)
-files = lapply(file_names, read.csv, header = TRUE)
+meal_file_names = list.files(pattern="*meal*", full.names = TRUE)
+meal_files = lapply(meal_file_names, read.csv, header = TRUE)
+
+snack_file_names = list.files(pattern="*snack*", full.names = TRUE)
+snack_files = lapply(snack_file_names, read.csv, header = TRUE)
 
 # Load 1 file for testing script
 AUR0029.1 <- read.csv("AUR0029.1_meal_bitesonly.csv", stringsAsFactors = FALSE)
@@ -23,9 +31,11 @@ AUR0029.1 <- read.csv("AUR0029.1_meal_bitesonly.csv", stringsAsFactors = FALSE)
 ## (1) Calculate the total time of the recording
 ## (2) Take an input interval (e.g. 2 seconds) and make a new dataframe (Interval.df) that contains a row for every interval within the recording (e.g., if recording is 30s and interval is 2s, B will contain 15 rows)
 ## (3) For each row in Interval.df (i.e., each interval), will look to input file to see if an event occured during that time; indicate TRUE or FALSE
+## (4) For each row in Interval.df (i.e., each interval), will look to input file to count how many events occured during that time; output count
+## (5) For each row in Interval.df (i.e., each interval), will look to input file to average IBI over the given interval (optional use: IBI = TRUE)
 
-event_in_interval <- function(data, unit, interval) {
-
+event_in_interval <- function(data, unit, interval, IBI) {
+  
   if (unit == "percentage") {
     cat("unit is percentage", "\n")
 
@@ -73,7 +83,7 @@ event_in_interval <- function(data, unit, interval) {
   # Make variable to indicate event count
   Interval.df$EventCount = NA
   
-  # For each interval, EventOccur = TRUE if there is any event in input data that occured between lower and upper threshold for that interval
+  # For each interval, set upper and lower thresholds, indicate EventOccur = TRUE if there is any event in input data that occured between the thresholds
   Intnums = unique(Interval.df$IntNum)
   for (Int in Intnums) {
     upperthresh = (Interval.df$upperthresh[Interval.df$IntNum == Int])
@@ -82,61 +92,88 @@ event_in_interval <- function(data, unit, interval) {
     eventcount = 0
     lastrow = nrow(data)
     
-  # Count how many events occured between lower and upperthresholds for each interval
+    if (IBI == TRUE) {
+      # Make an empty list 
+      IBIlist = c()
+    }
+    
+    # Reference each event from input dataset and count how many events occured between lower and upperthresholds for each interval
     for (eventnum in 1:nrow(data)) {
        if (eventnum != lastrow) {
          # For all intervals except the last, do not include the upper boundary
-        if (isTRUE(data$StartTime_seconds_adjusted[eventnum] >= lowerthresh & data$StartTime_seconds_adjusted[eventnum] < upperthresh)) {
-          eventcount = eventcount + 1
+          if (isTRUE(data$StartTime_seconds_adjusted[eventnum] >= lowerthresh & data$StartTime_seconds_adjusted[eventnum] < upperthresh)) {
+            eventcount = eventcount + 1
+            if (IBI == TRUE) {
+              IBIlist <- append(IBIlist, data$IBI[eventnum])
+            }
+          }
         }
-      }
       if (eventnum == lastrow) {
         # For the last interval, include the upper boundary
-        if (isTRUE(data$StartTime_seconds_adjusted[eventnum] >= lowerthresh & data$StartTime_seconds_adjusted[eventnum] <= upperthresh)) {
-          eventcount = eventcount + 1
-        }
+          if (isTRUE(data$StartTime_seconds_adjusted[eventnum] >= lowerthresh & data$StartTime_seconds_adjusted[eventnum] <= upperthresh)) {
+            eventcount = eventcount + 1
+            if (IBI == TRUE) {
+              IBIlist <- append(IBIlist, data$IBI[eventnum])
+            }
+          }
       }
     }
+    
+    # Add EventCount and IBI_avg to output dataframe    
     Interval.df$EventCount[Interval.df$IntNum == Int] = eventcount
+  
+    if (IBI == TRUE ) {
+      print(IBIlist)
+      Interval.df$IBI_avg[Interval.df$IntNum == Int] = mean(IBIlist, na.rm = TRUE)
+    }
   }
-    return(Interval.df)
+  
+  # Make cumulative sum variables
+  Interval.df$EventCount_cumulative <- cumsum(Interval.df$EventCount)
+  Interval.df$EventOccur_cumulative = cumsum(as.numeric(Interval.df$EventOccur))
+  
+  return(Interval.df)
 }
 
 #### Apply function; output individual or compiled dataframes ####
 
-event_in_interval(AUR0029.1, "percentage", 60)
+test <- event_in_interval(AUR0029.1, "percentage", 10, IBI = TRUE)
 
 # Set desired intervals
-intlist <- list(30, 60)
+intlist <- list(10)
+#unit = "time"
 unit = "percentage"
 
+## Modify loop to loop through both snack and meal files
 for (interval in intlist) { 
-  # Make temporary empty dataframe that all data will be added to
-  temp <- data.frame(matrix(ncol = 7, nrow = 0))
-    for (file in files) {
-      
-      if (nrow(file) == 0) {
-        print("no data")
-      }
-      
-      else if (nrow(file) > 1) {
-        sesnum <- (file$session[1])
-        parid <- print(file$parid[1], max.levels = 0)
-        output <- event_in_interval(file, unit, interval)
-        output$ParID <- parid
-        output$session <- sesnum
+    # Make temporary empty dataframe that all data will be added to
+    temp <- data.frame(matrix(ncol = 7, nrow = 0))
+      for (file in meal_files) {
+      #for (file in snack_files) {
+        #mealtype = (file$Paradigm[1])
+        if (nrow(file) == 0) {
+          print("no data")
+        }
         
-        ## Make separate dataframe for meal/participant data
-        #assign(paste(parid,sesnum,"EventInInt",interval,sep = "_"), event_in_interval(file, interval))
-        
-        # Add participant's data to compiled dataframe
-        temp <- rbind(temp, output)
+        else if (nrow(file) > 1) {
+          sesnum <- (file$session[1])
+          parid <- print(file$parid[1], max.levels = 0)
+          output <- event_in_interval(file, unit, interval, IBI = TRUE)
+          output$ParID <- parid
+          output$session <- sesnum
+          output$ID <- as.character(paste(output$ParID, output$session, sep = "_"))
+          
+          ## Make separate dataframe for meal/participant data
+          #assign(paste(parid,sesnum,"EventInInt",interval,sep = "_"), event_in_interval(file, interval))
+          
+          # Add participant's data to compiled dataframe
+          temp <- rbind(temp, output)
+        }
       }
-    }
-  
-  # Modify name of temporary dataframe
-  assign(paste0("Event_in_", unit, "_interval_", interval), temp)
-  rm(temp)
-  
+    
+    # Modify name of temporary dataframe
+    assign(paste0("Meal_Event_in_", unit, "_interval_", interval), temp)
+    #assign(paste0("Snack_Event_in_", unit, "_interval_", interval), temp)
+    rm(temp)
 }
 
